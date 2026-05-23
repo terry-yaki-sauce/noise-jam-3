b42ca707dd77710869bb93d122f832fc9ca00165
terry-yaki-sauce <281311365+terry-yaki-sauce@users.noreply.github.com>
2026-05-22 22:30:06 -0500

    add selector for dialogue type (simple vs. choice. Unity can suck my ***

--- Assets/Scripts/UI/DialogueLines.cs
+++ Assets/Scripts/UI/DialogueLines.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Dialogue;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
 
 namespace Dialogue
 {
   [CreateAssetMenu(fileName = "DialogueLines", menuName = "Scriptable Objects/DialogueLines")]
   public class DialogueLines : ScriptableObject
   {
-    [SerializeField] private List<DialogueNode> dialogueNodes;
+    [SerializeReference] private List<DialogueNode> dialogueNodes;
     public List<DialogueNode> DialogueNodes { get => dialogueNodes; }
   }
 
@@ -16,9 +19,22 @@ namespace Dialogue
   {
     [SerializeField] private string text;
     public string Text { get => text; }
-    
+
     [SerializeField] private Character characterToShow;
     public Character CharacterToShow { get => characterToShow; }
+  }
+
+  [System.Serializable]
+  public class DialogueChoiceNode : DialogueNode
+  {
+    [SerializeField] private List<Choice> choices;
+  }
+
+  [System.Serializable]
+  public struct Choice
+  {
+    public string text;
+    public int jumpIndex;
   }
 
   public enum Character
@@ -26,4 +42,29 @@ namespace Dialogue
     Lucy,
     NPC
   }
+
+
 }
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(DialogueLines),true)]
+public class DialogueLinesEditor : Editor
+{
+  public override void OnInspectorGUI()
+  {
+    DrawDefaultInspector();
+
+    DialogueLines myTarget = (DialogueLines) target;
+
+    if (GUILayout.Button("Add Regular Node"))
+    {
+      myTarget.DialogueNodes.Add(new DialogueNode());
+    }
+    if (GUILayout.Button("Add Choice Node"))
+    {
+      myTarget.DialogueNodes.Add(new DialogueChoiceNode());
+    }
+  }
+}
+#endif
